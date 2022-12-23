dbbbde82e112ca96bcc31aa6235301fd6352991a
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2022-12-23 07:23:16 +0100

    base App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
 	<StrictMode>
-		<h1>Hello here !</h1>
+		<App />
 	</StrictMode>
 );
