945d9236e5f244d12a46444c1806f6a5ed908632
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2023-01-19 19:09:02 +0100

    get sample Data

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -3,9 +3,12 @@ import React, { useEffect, useState } from "react";
 export default function Quizz() {
 	const [data, setData] = useState();
 	useEffect(() => {
-		const tempData = fetch("https://opentdb.com/api.php?amount=5").then((res) =>
-			console.log(res)
-		);
+		const tempData = fetch("https://opentdb.com/api.php?amount=5")
+			.then((res) => res.json())
+			.then((dataInput) => setData(dataInput));
+		console.log(data);
 	}, []);
+	console.log(`Outside effect `);
+	console.log(JSON.stringify(data));
 	return <div className="quizz"></div>;
 }
