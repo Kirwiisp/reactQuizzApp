3413a25c61055f804d77c0f1e7bef848593c0e4d
Kirwiisp <59315476+Kirwiisp@users.noreply.github.com>
2023-01-21 22:45:10 +0100

    set button style and conditionnal rendering. TODO Limit number of clic/ change footer when finished / Add score

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export default function Question({ question, answerArray }) {
+export default function Question({ question, answerArray, handleClick }) {
+	console.log("render question ");
+	console.log(answerArray[0].isClicked === true);
 	const resp = answerArray.map((e, index) => (
-		<button className="button-answer" key={e + index}>
-			{e}
+		<button
+			className={`button-answer ${e.isClicked}`}
+			key={e.answer + index}
+			onClick={() => handleClick(question, index)}
+		>
+			{e.answer}
 		</button>
 	));
 	return (
--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -7,6 +7,9 @@ export default function Quizz() {
 	/**
 	 * Fetch questions data
 	 * TODO : uncomment, temporary use of dataExemple
+	 *
+	 * API url : https://opentdb.com/api_config.php
+	 *
 	 */
 
 	// useEffect(() => {
@@ -16,23 +19,85 @@ export default function Quizz() {
 	// 	console.log(data);
 	// }, []);
 
+	console.log("render Quizz");
+
 	useEffect(() => {
+		const isClicked = "";
 		setData(
 			dataJson.results.map((e) => ({
 				question: e.question,
-				array: [e.correct_answer, ...e.incorrect_answers],
+				correctAnswer: e.correct_answer,
+				array: shuffleArray([
+					{ answer: e.correct_answer, isClicked: isClicked },
+					...e.incorrect_answers.map((e) => ({
+						answer: e,
+						isClicked: isClicked,
+					})),
+				]),
 			}))
 		);
 	}, []);
 
+	function shuffleArray(arr) {
+		const tempArr = arr.map((e) => e);
+		for (let i = tempArr.length - 1; i > 0; i--) {
+			let randIndex = Math.floor(Math.random() * tempArr.length);
+			let temp = tempArr[i];
+			tempArr[i] = tempArr[randIndex];
+			tempArr[randIndex] = temp;
+		}
+		return tempArr;
+	}
+	data.forEach((e) => console.log(shuffleArray(e.array)));
+
+	function handleClick(question, inputIndex) {
+		setData((prevData) =>
+			prevData.map((e) => ({
+				...e,
+				array:
+					question !== e.question
+						? e.array
+						: e.array.map((ob, index) => ({
+								...ob,
+								isClicked: index !== inputIndex ? "" : "clicked",
+						  })),
+			}))
+		);
+	}
+
+	function checkAnswers() {
+		console.log("check");
+		setData((prevData) =>
+			prevData.map((e) => ({
+				...e,
+				array: e.array.map((ans) => ({
+					...ans,
+					isClicked:
+						ans.answer === e.correctAnswer
+							? "good"
+							: ans.isClicked === "clicked"
+							? "wrong"
+							: "faded",
+				})),
+			}))
+		);
+	}
+
 	const questionsToRender = data.map((e) => (
-		<Question question={e.question} answerArray={e.array} key={e.question} />
+		<Question
+			question={e.question}
+			answerArray={e.array}
+			key={e.question}
+			handleClick={handleClick}
+		/>
 	));
 
 	return (
 		<div className="quizz">
 			{questionsToRender}
-			<button className="button-check button-main">Check Answers</button>
+			<button className="button-check button-main" onClick={checkAnswers}>
+				Check Answers
+			</button>
 		</div>
 	);
 }
